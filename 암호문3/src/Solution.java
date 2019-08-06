1d895084d5a871418cfcac5611cc097b919bc962
DDD <dhzm2aud@naver.com>
2019-08-06 17:50:22 +0900

    algo hw 08

---
+++ 암호문3/src/Solution.java
@@ -0,0 +1,68 @@
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+	public static void main(String[] args) throws Exception{
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int T = 10;
+		for(int tNum = 1; tNum <= T ; tNum++)
+		{
+			int cryptNum = Integer.parseInt(br.readLine());
+			ArrayList<String> list = new ArrayList<>();
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int i = 0; i < cryptNum ; i++)
+				list.add(st.nextToken());
+			int commandNum = Integer.parseInt(br.readLine());
+			st = new StringTokenizer(br.readLine());
+			for(int i = 0 ; i < commandNum ; i++)
+			{
+				String command = st.nextToken();
+				switch(command)
+				{
+				case"I":
+					int index = Integer.parseInt(st.nextToken());
+					int num = Integer.parseInt(st.nextToken());
+					for(int j = 0 ; j < num ; j++)
+					{
+						String tmp = st.nextToken();
+						list.add(index+j, tmp);
+					}
+					break;
+				case"D":
+					index = Integer.parseInt(st.nextToken());
+					num = Integer.parseInt(st.nextToken());
+					for(int j = 0 ; j < num; j++)
+					{
+						list.remove(index);
+					}
+					break;
+				case"A":
+					num = Integer.parseInt(st.nextToken());
+					for(int j = 0 ; j < num ;j++)
+					{
+						String tmp = st.nextToken();
+						list.add(tmp);
+					}
+					break;
+				}
+			}
+			bw.write("#"+tNum+" ");
+			for(int i = 0 ; i < 10 ; i++)
+				bw.write(list.get(i)+" ");
+			bw.write("\n");
+		}//for tNum
+		bw.flush();
+		bw.close();
+	}
+
+}
