677af3685a33a8daff2f9157395f584dded65a53
seongjun <dhzm2aud@naver.com>
2019-08-13 01:37:12 +0900

    dfs

--- 최대 상금/src/Solution.java
+++ 최대 상금/src/Solution.java
@@ -5,29 +5,60 @@ import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Solution {
-
+	public static String output;
 	public static void main(String[] args) throws Exception{
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = null;
-		
+
 		int T = Integer.parseInt(br.readLine());
 		for(int tNum = 1 ; tNum <= T ; tNum++)
 		{
+			st = new StringTokenizer(br.readLine());
 			bw.write("#"+tNum+" ");
-			String tmp = st.nextToken();
-			int[] arr = new int[tmp.length()];
-			for(int i = 0 ; i < tmp.length() ; i++)
-				arr[i] = tmp.charAt(i)-'0';
-			int swap = Integer.parseInt(st.nextToken());
-			
-			
-			
+			String str = st.nextToken();
+			int change = Integer.parseInt(st.nextToken());
+			output = str;
+			boolean pick[][] = new boolean[999_999][10];
+			permutation(0,str,pick,0,change);
+
+			bw.write(output + "\n");
 		}//for tNum
 		bw.flush();
 		bw.close();
-		
+
+	}
+
+	public static void permutation(int start,String newStr,boolean[][] pick, int curCnt, int targetCnt)
+	{
+//		System.out.println("newStr : " + newStr + ", start : " + start + ", curCnt" + curCnt);
+		if(curCnt == targetCnt)
+		{
+			if(output.compareTo(newStr) < 0)
+				output = newStr;
+			return ;
+		}
+
+		if(pick[Integer.parseInt(newStr)][curCnt])
+			return;
+		pick[Integer.parseInt(newStr)][curCnt] = true;
+		char[] charList = newStr.toCharArray();
+		for(int i = start ; i < charList.length; i++)
+		{
+			for(int j = i+1 ; j < charList.length ; j++)
+			{
+					swap(charList,i,j);
+					permutation(i,new String(charList), pick,curCnt+1,targetCnt);
+					swap(charList,i,j);
+			}
+		}//for i
+	}
+	public static void swap(char[] list, int a, int b)
+	{
+		char tmp = list[a];
+		list[a] = list[b];
+		list[b] = tmp;
 	}
 
 }
