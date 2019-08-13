ceb35d2768f3417446f9f0ce3b38d601aced4cbc
DDD <dhzm2aud@naver.com>
2019-08-13 10:59:58 +0900

    my style code

--- 최대 상금/src/Solution.java
+++ 최대 상금/src/Solution.java
@@ -19,18 +19,17 @@ public class Solution {
 			bw.write("#"+tNum+" ");
 			String str = st.nextToken();
 			int change = Integer.parseInt(st.nextToken());
-			output = str;
-			boolean pick[][] = new boolean[999_999][10];
-			permutation(0,str,pick,0,change);
+			output = "";
+			boolean pick[][] = new boolean[1_000_000][change];
+			permutation(str,pick,0,change);
 
 			bw.write(output + "\n");
 		}//for tNum
 		bw.flush();
 		bw.close();
-
 	}
 
-	public static void permutation(int start,String newStr,boolean[][] pick, int curCnt, int targetCnt)
+	public static void permutation(String newStr,boolean[][] pick, int curCnt, int targetCnt)
 	{
 //		System.out.println("newStr : " + newStr + ", start : " + start + ", curCnt" + curCnt);
 		if(curCnt == targetCnt)
@@ -39,18 +38,22 @@ public class Solution {
 				output = newStr;
 			return ;
 		}
-
 		if(pick[Integer.parseInt(newStr)][curCnt])
 			return;
 		pick[Integer.parseInt(newStr)][curCnt] = true;
+
+
 		char[] charList = newStr.toCharArray();
-		for(int i = start ; i < charList.length; i++)
+		for(int i = 0 ; i < charList.length; i++)
 		{
-			for(int j = i+1 ; j < charList.length ; j++)
+			for(int j = 0 ; j < charList.length ; j++)
 			{
+				if(i != j)
+				{
 					swap(charList,i,j);
-					permutation(i,new String(charList), pick,curCnt+1,targetCnt);
+					permutation(new String(charList), pick,curCnt+1,targetCnt);
 					swap(charList,i,j);
+				}
 			}
 		}//for i
 	}
@@ -60,5 +63,4 @@ public class Solution {
 		list[a] = list[b];
 		list[b] = tmp;
 	}
-
 }
