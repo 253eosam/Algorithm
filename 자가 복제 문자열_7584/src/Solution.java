622914103ba6f8064e235ca1b935323bd58668d5
seongjun <dhzm2aud@naver.com>
2019-08-15 20:41:51 +0900

    이진탐색을 이용한 방법으로 속도를 개선

--- 자가 복제 문자열_7584/src/Solution.java
+++ 자가 복제 문자열_7584/src/Solution.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class Solution {
-	public static String specialStr = "0";
+//	public static String specialStr = "0";
 	public static void main(String[] args) throws Exception 
 	{
 
@@ -15,9 +15,11 @@ public class Solution {
 		
 		for(int tNum = 1 ; tNum <= T ; tNum++)
 		{
-			int N = Integer.parseInt(br.readLine());
+			long N = Long.parseLong(br.readLine());
 			
+			int output = binarySearch(N);
 			
+			bw.write("#"+tNum+" "+output+"\n");
 			
 //			while(N > specialStr.length()-1)
 //			{
@@ -31,31 +33,63 @@ public class Solution {
 	
 	}
 	
-	public static String copyP(String str)
+	public static int binarySearch(long target)
 	{
-		String output = "";
+		int solv = 0;
 		
-		output += str + "0" + reverse(str);
+		long left = 1 , right = 1;
 		
-		return output;
+		while(right <= target)	// find right
+			right <<= 1;
+		
+		while(true)
+		{
+			long mid = (left + right) / 2;
+			
+			if(target == mid) break;
+			else
+			{
+				if(target < mid)
+				{
+					solv = 0;
+					right = mid;
+				}
+				else
+				{
+					solv = 1;
+					left = mid;
+				}
+			}
+		}
+		
+		return solv;
 	}
-	public static String reverse(String str)
-	{
-		String output = "";
-		
-		char[] tmp = reflect(str).toCharArray();
-		for(char t : tmp)
-			output += t == '0' ? '1' : '0';
-		
-		return output;
-	}
-	public static String reflect(String str)
-	{
-		String output = "";
-		
-		for(int i = str.length()-1 ; i >= 0 ; i--)
-			output += str.charAt(i);
-		
-		return output;
-	}
+	
+//	public static String copyP(String str)
+//	{
+//		String output = "";
+//		
+//		output += str + "0" + reverse(str);
+//		
+//		return output;
+//	}
+//	public static String reverse(String str)
+//	{
+//		String output = "";
+//		
+//		char[] tmp = reflect(str).toCharArray();
+//		for(char t : tmp)
+//			output += t == '0' ? '1' : '0';
+//		
+//		return output;
+//	}
+//	public static String reflect(String str)
+//	{
+//		String output = "";
+//		
+//		for(int i = str.length()-1 ; i >= 0 ; i--)
+//			output += str.charAt(i);
+//		
+//		return output;
+//	}
 }
