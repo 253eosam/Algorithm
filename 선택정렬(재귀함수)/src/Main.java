42e4258d1e10ec14577cbb13d828d19b6a6bedf6
DDD <dhzm2aud@naver.com>
2019-08-19 11:47:52 +0900

    selected sort using recursion

---
+++ 선택정렬(재귀함수)/src/Main.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = new int[] {1,0,8,5,2,7,4,1,9,6,3,10};
+
+		for(int i = 0 ; i < arr.length ; i++)
+		{
+			int tmp = 0;
+			tmp = selectSortRecursion(arr, i);
+			swap(arr,i,tmp);
+		}
+		
+		System.out.println(Arrays.toString(arr));
+	}
+	public static int selectSortRecursion(int[] arr, int depth)
+	{
+		if(arr.length-1 == depth) return depth;
+		
+		int minimumIndex = depth;
+			
+		int tmp = selectSortRecursion(arr, depth+1);
+		
+		minimumIndex = arr[tmp] < arr[depth] ? tmp : depth;
+		
+		return minimumIndex;
+	}
+	public static void swap(int[] arr , int a, int b)
+	{
+		int tmp = arr[a];
+		arr[a] = arr[b];
+		arr[b] = tmp;
+	}
+
+}
