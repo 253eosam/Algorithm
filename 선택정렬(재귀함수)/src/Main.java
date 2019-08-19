ce160cb8d21581635d308de7f3d604f60d3de670
DDD <dhzm2aud@naver.com>
2019-08-19 17:02:50 +0900

    선택정렬 재귀로 접근

--- 선택정렬(재귀함수)/src/Main.java
+++ 선택정렬(재귀함수)/src/Main.java
@@ -6,27 +6,32 @@ public class Main {
 		// TODO Auto-generated method stub
 		int[] arr = new int[] {1,0,8,5,2,7,4,1,9,6,3,10};
 
+		selectedSort(arr);
+		
+		System.out.println(Arrays.toString(arr));
+	}
+	
+	public static void selectedSort(int[] arr)
+	{
 		for(int i = 0 ; i < arr.length ; i++)
 		{
 			int tmp = 0;
 			tmp = selectSortRecursion(arr, i);
 			swap(arr,i,tmp);
 		}
-		
-		System.out.println(Arrays.toString(arr));
 	}
+	
 	public static int selectSortRecursion(int[] arr, int depth)
 	{
 		if(arr.length-1 == depth) return depth;
 		
 		int minimumIndex = depth;
-			
 		int tmp = selectSortRecursion(arr, depth+1);
-		
 		minimumIndex = arr[tmp] < arr[depth] ? tmp : depth;
 		
 		return minimumIndex;
 	}
+	
 	public static void swap(int[] arr , int a, int b)
 	{
 		int tmp = arr[a];
