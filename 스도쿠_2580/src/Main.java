8964a6d3cae9b0ef1e10696f3eb7bfda62e7d5d2
ssafy_Seongjun <dhzm2aud@naver.com>
2019-09-30 10:39:05 +0900

    미풀이

---
+++ 스도쿠_2580/src/Main.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static BufferedReader br;
+	public static BufferedWriter bw;
+	public static StringTokenizer st;
+	public static int stoi(String str) { return Integer.parseInt(str); }
+	public static void main(String[] args) throws Exception{
+	}
+
+}
