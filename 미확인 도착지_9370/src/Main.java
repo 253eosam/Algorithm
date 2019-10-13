593673db1bdf39aff1f5bcb9a83dbb736447a80b
seongjun2 <dhzm2aud@naver.com>
2019-10-14 02:21:28 +0900

    다익스트라

--- 미확인 도착지_9370/src/Main.java
+++ 미확인 도착지_9370/src/Main.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
@@ -15,11 +16,12 @@ public class Main {
 	public static int n,m,t;	// 교차로, 도로, 목적지 후보의  개수
 	public static int s,g,h;	// 예술가 출발지, 교차로 양 쪽의 정점
 	public static int a,b,d;	// a 와 b 사이의 양뱡향 도로
-	public static ArrayList<Integer> targets;	//목적지 후도들
-	public static int[] dis;
+	public static ArrayList<Integer> targets;	//목적지 후보들
+	public static int[] dis,dis2;
 	public static PriorityQueue<Edge> pq;
 	public static ArrayList<Edge>[] arr;
 	public static final int INF = Integer.MAX_VALUE;
+	public static int ghDis, fIdx , sIdx, allDis;	// g에서 h까지의 거리, s에서 g or h중 가까운 곳을 fIdx , 먼곳을 sIdx로 잡음, allDis = s -> fIdx -> sIdx
 	public static class Edge implements Comparable<Edge>
 	{
 		int idx,w;
@@ -32,7 +34,10 @@ public class Main {
 		public int compareTo(Edge o) {
 			return this.w-o.w;
 		}
-		
+		@Override
+		public String toString() {
+			return "Edge [idx=" + idx + ", w=" + w + "]";
+		}
 	}
 	public static void main(String[] args) throws Exception 
 	{
@@ -42,11 +47,67 @@ public class Main {
 		{
 			init();
 			
+			makeInfi(dis,s);
+			dijkstra(dis,s);
+			findFastMidVertex();
+			
+			allDis = dis[fIdx] + ghDis;
+			
+			makeInfi(dis2,sIdx);
+			dijkstra(dis2,sIdx);
+			
+			print();
+	
+			
 		}//for tNum
+		bw.close();
 	}
-	public static void makeInfi(int start)
+	public static void print() throws IOException
 	{
-		dis = new int[n+1];
+		for(int i : targets)
+		{
+			if(allDis + dis2[i] == dis[i])
+			{
+				bw.write(i + " ");
+			}
+		}
+		bw.write("\n");
+	}
+	public static void findFastMidVertex()
+	{
+		if(dis[g] < dis[h])
+		{
+			fIdx = g;
+			sIdx = h;
+		}
+		else
+		{
+			fIdx = h;
+			sIdx = g;
+		}
+	}
+	public static void dijkstra(int[] dis, int start)
+	{
+		pq.add(new Edge(start,0));
+		
+		Edge cur = null;
+		while(!pq.isEmpty())
+		{
+			cur = pq.poll();
+			
+			for(Edge next : arr[cur.idx])
+			{
+				if(dis[next.idx] > dis[cur.idx] + next.w)
+				{
+					pq.add(new Edge(next.idx, dis[cur.idx] + next.w));
+					dis[next.idx] = dis[cur.idx] + next.w;
+				}
+				
+			}
+		}
+	}
+	public static void makeInfi(int[] dis, int start)
+	{
 		for(int i = 0 ; i <= n ; i++)
 			dis[i] = INF;
 		dis[start] = 0;
@@ -76,12 +137,18 @@ public class Main {
 			d = Integer.parseInt(st.nextToken());
 			arr[a].add(new Edge(b,d));
 			arr[b].add(new Edge(a,d));
+			if((a == g && b == h) || (a == h && b == g))
+				ghDis = d;
 		}
 		
 		targets = new ArrayList<>();
 		for(int i = 0 ; i < t ; i++)
 			targets.add(Integer.parseInt(br.readLine()));
 		
+		Collections.sort(targets);
+		
+		dis = new int[n+1];
+		dis2 = new int[n+1];
 	}
 
 }
