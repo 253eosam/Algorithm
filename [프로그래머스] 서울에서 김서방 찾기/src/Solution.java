6ff87d1918f9f95b7eb61cf5e2d143c1d12d06f0
Seongjun Lee <41896147+dhzm2aud@users.noreply.github.com>
2020-01-03 18:35:58 +0900

    배열에서 원하는 값 찾기

--- [프로그래머스] 서울에서 김서방 찾기/src/Solution.java
+++ [프로그래머스] 서울에서 김서방 찾기/src/Solution.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 class Solution {
   public String solution(String[] seoul) {
       int answer = 0;
@@ -12,4 +14,12 @@ class Solution {
       
       return "김서방은 " + answer + "에 있다";
   }
-}
+  
+  
+  public String findKim(String[] seoul){
+      //x에 김서방의 위치를 저장하세요.
+      int x = Arrays.asList(seoul).indexOf("Kim");
+
+      return "김서방은 "+ x + "에 있다";
+  }
+}	
