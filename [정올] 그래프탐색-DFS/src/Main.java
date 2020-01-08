2d41f8b5d85343ce5bf67c4eb49a2ccfbd5febed
seongjun2 <dhzm2aud@naver.com>
2020-01-09 05:09:53 +0900

    불 5427

---
+++ [정올] 그래프탐색-DFS/src/Main.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/*
+ * Á¤¿Ã : N Queen 1889
+ */
+public class Main {
+	public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static int stoi(String str) 	{ return Integer.parseInt(str); }
+
+	public static void main(String[] args) {
+
+	}
+
+}
