2774813e54f748286f6367264524c28365d9caae
이성준 <dhzm2aud@naver.com>
2020-05-16 18:31:06 +0900

    bfs , bs

--- Problem/[백준] 중량제한/src/Main.java
+++ Problem/[백준] 중량제한/src/Main.java
@@ -1,74 +1,85 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
- 
-public class Main {
- 
-    static boolean check(List<Weight>[] a, int limit, int start, int end, boolean [] b) {
-        if(b[start])
-            return false;
-        b[start] = true;
-        if(start == end)
-            return true;
-        for(Weight v : a[start]) {
-            if(v.g >= limit) {  //입력받은 중량으로 이동할 수 있는지 체크
-                //return check(a, limit, v.v, end, b) 
-                //이렇게 하면 check가 false 한번이라도 되면 for문이 끝나버린다. 헤맨 부분
-                if(check(a, limit, v.v, end, b)){ //if문으로 해야 for문이 끝나지 않고 계속 돈다.
-                    return true;
-                }
-            }
-        }
-        
-        return false;
-    }
- 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt(); 
-        int m = scan.nextInt();
-        List<Weight> [] a = (List<Weight> []) new ArrayList[n+1];
- 
-        for(int i=1; i<=n; i++)
-            a[i] = new ArrayList<>();
- 
-        int max = 0;
-        for(int i=0; i<m; i++) {
-            int v1 = scan.nextInt();
-            int v2 = scan.nextInt();
-            int g = scan.nextInt();
-            a[v1].add(new Weight(v2, g));
-            a[v2].add(new Weight(v1, g));
-            max = Math.max(g, max);
-        }
-        int tx = scan.nextInt();
-        int ty = scan.nextInt();
- 
- 
- 
-        int left = 1;
-        int right = 1_000_000_000;
-        int answer = 0;
-        while(left<=right) {
-            boolean []b = new boolean[n+1];
-            int mid = (left+right)/2;
-            if(check(a, mid, tx, ty, b)) {
-                left = mid+1;
-            }
-            else
-                right = mid-1;
-        }
-        answer = right;
-        System.out.println(answer);
-    }
- 
-}
-class Weight{
-    int v;
-    int g;
- 
-    public Weight(int v, int g) {
-        this.v = v;
-        this.g = g;
-    }
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main{
+	public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static final int MAXIMUM = 1_000_000_000;
+	
+	public static int N,M,ts,te;
+	public static ArrayList<Pos>[] list;
+	public static class Pos{
+		int x, w;
+		public Pos(int x, int w) {
+			super();
+			this.x = x;
+			this.w = w;
+		}
+	}
+	public static void main(String[] args) throws IOException {
+		input();
+		logic();
+	}
+	private static void logic() throws IOException {
+		bw.append(String.valueOf(bs()));
+		bw.close();
+	}
+	public static int bs() {
+		int left = 1;
+		int right = MAXIMUM;
+		
+		while(left <= right) {
+			int mid = (left+right)/2;
+			if(!bfs(mid)) right = mid-1;
+			else left = mid+1;
+		}
+		
+		return right;
+	}
+	public static boolean bfs(int val) {
+		boolean[] visited = new boolean[N+1];
+		Queue<Pos> q = new LinkedList<>();
+		q.add(new Pos(ts,0));
+		visited[ts] = true;
+		
+		while(!q.isEmpty()) {
+			Pos p = q.poll();
+			if(p.x == te) return true;
+			for(Pos tmp : list[p.x]) {
+				if(val <= tmp.w && !visited[tmp.x]) {
+					q.add(tmp);
+					visited[tmp.x] = true;
+				}
+			}
+		}
+		
+		return false;
+	}
+	private static void input() throws IOException {
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		list=new ArrayList[N+1];
+		for(int i = 1 ; i <= N ; i++) list[i] = new ArrayList<>();
+		
+		for(int i = 0 ; i < M ; i++) {
+			st= new StringTokenizer(br.readLine());
+			int s = Integer.parseInt(st.nextToken());
+			int e = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
+			list[s].add(new Pos(e,w));
+			list[e].add(new Pos(s,w));
+		}
+		st=new StringTokenizer(br.readLine());
+		ts = Integer.parseInt(st.nextToken());
+		te = Integer.parseInt(st.nextToken());
+	}
 }
